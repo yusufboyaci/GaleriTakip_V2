7d580ba1bd97aac18e197dba47d53ae027a37010
yusufboyaci <boyacyusuf@gmail.com>
2022-09-23 18:04:26 +0300

    Add project files.

---
+++ Core/Entity/CoreEntity.cs
@@ -0,0 +1,24 @@
+﻿using Core.Enum;
+using Core.Methods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entity
+{
+    public class CoreEntity : IEntity<Guid>
+    {
+        NetworkFuntions networkFuntions = new NetworkFuntions();
+        public CoreEntity()
+        {
+
+        }
+        public Guid Id { get; set; }
+        public Guid? MasterId { get; set; } //Üst Id demektir.Yetki vermede kullanılacaktır.
+        public Status Status { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+    }
+}
