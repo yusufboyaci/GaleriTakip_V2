c7c980bbea35a2cdb42818ccf3cade272f700b23
yusufboyaci <boyacyusuf@gmail.com>
2022-11-02 18:19:40 +0300

    ---

---
+++ DataAccess/ViewModels/TableOfTheCarAndGallery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ViewModels
+{
+    public class TableOfTheCarAndGallery
+    {
+        public Guid Id { get; set; }
+        public string? GalleryName { get; set; }
+        public string? GalleryAddress { get; set; }
+        public string? CarName { get; set; }
+        public decimal? Price { get; set; }
+        public int? Stock { get; set; }
+        public string? FilePath { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+//wwroot
+app.UseStaticFiles();
+
+//node_modules
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+    RequestPath = new PathString("/vendor")
+});
+
+
+app.UseAuthentication();
 
 app.Run();
