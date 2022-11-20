652280922bd9a4bd605aad82b2f551c7934c4018
yusufboyaci <boyacyusuf@gmail.com>
2022-11-20 17:29:58 +0300

    cars controller yapıldı. 20112022 1729

---
+++ CarApi/Controllers/CarsController.cs
@@ -0,0 +1,120 @@
+﻿using DataAccess.Context;
+using DataAccess.Repositories.Concrete;
+using DataAccess.ViewModels;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        CarRepository _carRepository;
+        CarGalleryRepository _galleryRepository;
+        public CarsController(AppDbContext context)
+        {
+            _carRepository = new CarRepository(context);
+            _galleryRepository = new CarGalleryRepository(context);
+        }
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            try
+            {
+                List<Car> cars = _carRepository.GetActives();
+                return Ok(cars);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("Get")]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                return Ok(_carRepository.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(Car car)
+        {
+            try
+            {
+                _carRepository.Add(car);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("Update")]
+        public IActionResult Update(Car car)
+        {
+            try
+            {
+                _carRepository.Update(car);
+                _carRepository.Activate(car.Id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("Delete")]
+        public IActionResult Delete(Car car)
+        {
+            try
+            {
+                _carRepository.Remove(car);
+                return NoContent();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet("ListCarsAndGaleries")]
+        public IActionResult ListCarsAndGaleries()
+        {
+            try
+            {
+                List<CarGallery> galeries = _galleryRepository.GetActives();
+                List<Car> cars = _carRepository.GetActives();
+
+                List<TableOfTheCarAndGallery> list = (
+                    from g in galeries
+                    join c in cars
+                    on g.Id equals c.CarGalleryId
+                    select new TableOfTheCarAndGallery
+                    {
+                        Id = c.Id,
+                        GalleryName = g.Name,
+                        GalleryAddress = g.Address,
+                        CarName = c.Name,
+                        Price = c.Price,
+                        Stock = c.Stock,
+                        FilePath = c.FilePath,
+                        Description = c.Descripton
+                    }
+                    ).ToList();
+                return Ok(list);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
