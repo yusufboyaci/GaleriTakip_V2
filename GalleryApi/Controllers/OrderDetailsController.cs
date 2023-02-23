891d532a852ab8da4486dfbae88e97f77ddbda01
yusufboyaci <boyacyusuf@gamil.com>
2023-02-23 21:40:36 +0300

    ------

--- GalleryApi/Controllers/OrderDetailsController.cs
+++ GalleryApi/Controllers/OrderDetailsController.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Context;
 using DataAccess.Repositories.Concrete;
+using EntityLayer.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,68 @@ namespace GalleryApi.Controllers
         {
             _orderDetailRepository = new OrderDetailRepository(context);
         }
-
+        public IActionResult List()
+        {
+            try
+            {
+                return Ok(_orderDetailRepository.GetActives());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("Get")]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                return Ok(_orderDetailRepository.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(OrderDetail orderDetail)
+        {
+            try
+            {
+                _orderDetailRepository.Add(orderDetail);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("Update")]
+        public IActionResult Update(OrderDetail orderDetail)
+        {
+            try
+            {
+                _orderDetailRepository.Update(orderDetail);
+                _orderDetailRepository.Activate(orderDetail.Id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("Delete")]
+        public IActionResult Delete(OrderDetail orderDetail)
+        {
+            try
+            {
+                _orderDetailRepository.Remove(orderDetail);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
